3c8ad0c8242cb23101eb38730abc8a859cbecc31
yushmanth <yushmanth@yushmanth-pc.com>
2023-07-05 19:25:08 +0530

    sockets implemented

--- index.js
+++ index.js
@@ -6,11 +6,14 @@ import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.js";
 import userRoutes from "./routes/user.js";
 import messageRoutes from "./routes/messages.js";
-import { Socket } from "socket.io";
+import { Server } from "socket.io";
+import { createServer } from "http";
+import { log } from "console";
 
 const app = express();
 dotenv.config();
 
+// const httpServer = createServer(app);
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({ origin: true, credentials: true }));
@@ -30,11 +33,33 @@ app.use((err, req, res, next) => {
   });
 });
 
-app.listen(process.env.PORT, (err) => {
+const httpServer = app.listen(process.env.PORT, (err) => {
   if (err) {
     console.log(err);
   } else {
     connect();
     console.log("connected to backend!");
   }
+});
+const io = new Server(httpServer, {
+  cors: {
+    origin: "http://localhost:3000",
+    credentials: true,
+    methods: ["GET", "POST"],
+  },
+});
+global.onlineUsers = new Map();
+
+io.on("connection", (socket) => {
+  global.chatSocket = socket;
+  socket.on("add-user", (userId) => {
+    onlineUsers.set(userId, socket.id);
+  });
+  console.log(onlineUsers);
+  socket.on("send-msg", (data) => {
+    const sendUserSocket = onlineUsers.get(data.to);
+    if (sendUserSocket) {
+      socket.to(sendUserSocket).emit("msg-recieve", data.message);
+    }
+  });
 });
